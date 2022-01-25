4a8a6639b70cf97e409841785c79cde616de0627
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2022-01-25 15:18:58 -0600

    Add files via upload

---
+++ conver_und.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 14:34:03 2022
+
+@author: dr.guillermo
+"""
+
+print("Conversión de Unidades")
+pulgadas= input("Da Pulgadas: ")
+cm=float(pulgadas)*2.54
+print("cm = ", cm)
+
+
+cm = input("Da los centimetros: ")
+pulg= float(cm)/2.54
+print("Pulgadas = ", pulg)
